12563e87b10cb8669d4676327b7b1b10bd8413db
rckc68 <rckc68@mail.missouri.edu>
2016-11-07 09:58:49 -0600

    Add files via upload

---
+++ stubs.js
@@ -0,0 +1,23 @@
+/* Stubs */
+
+/* Authentication */
+function registerAccount(username, password, firstName, lastName, email){}
+function login(username, password){}
+function logout(){}
+//function deactivateAccount(password){} ?
+//function changeUsername(username){} ?
+//function changePassword(password){} ?
+
+/* Message */
+function postMessage(message){}
+function updateMessage(messageId, message){}
+function deleteMessage(messageId){}
+
+/* Manifest */
+function uploadManifest(filename){}
+function editManifest(manifestId, filename){}
+function downloadManifest(manifestId){}
+function favoriteManifest(manifestId){}
+function unfavoriteManifest(manifestId){}
+//function createJupyterH(???????????){} ?
+//function spawnJupyterH(????????????){} ?
