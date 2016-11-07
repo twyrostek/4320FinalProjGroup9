332a2d10bf0d0119e0ca7df79ff888452dd4151a
rckc68 <rckc68@mail.missouri.edu>
2016-11-07 10:02:03 -0600

    Update stubs.js

--- stubs.js
+++ stubs.js
@@ -1,23 +1,33 @@
 /* Stubs */
 
 /* Authentication */
-function registerAccount(username, password, firstName, lastName, email){}
-function login(username, password){}
-function logout(){}
+function registerAccount(username, password, firstName, lastName, email) {}
+
+function login(username, password) {}
+
+function logout() {}
+
 //function deactivateAccount(password){} ?
 //function changeUsername(username){} ?
 //function changePassword(password){} ?
 
 /* Message */
-function postMessage(message){}
-function updateMessage(messageId, message){}
-function deleteMessage(messageId){}
+function postMessage(message) {}
+
+function updateMessage(messageId, message) {}
+
+function deleteMessage(messageId) {}
 
 /* Manifest */
-function uploadManifest(filename){}
-function editManifest(manifestId, filename){}
-function downloadManifest(manifestId){}
-function favoriteManifest(manifestId){}
-function unfavoriteManifest(manifestId){}
+function uploadManifest(filename) {}
+
+function editManifest(manifestId, filename) {}
+
+function downloadManifest(manifestId) {}
+
+function favoriteManifest(manifestId) {}
+
+function unfavoriteManifest(manifestId) {}
+
 //function createJupyterH(???????????){} ?
 //function spawnJupyterH(????????????){} ?
