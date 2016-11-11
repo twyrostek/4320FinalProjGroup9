d57624791825d63e0daa09d39476c04f5ef9ed24
Christopher King <cakd47@mail.missouri.edu>
2016-11-11 14:50:47 -0600

    upload.php

---
+++ upload.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    //New connection to the database
+    $connection = new MongoClient();
+
+    //Handle to the ocdx database
+    $db = $connection->ocdx;
+
+    //Handle to the collection
+    $manifest_collection = $db->manifest;
+
+    $cursor = $manifest_collection->find();
+?>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Simple Sidebar - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        OCDX eXchange
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">Search</a>
+
+                </li>
+                <li>
+                    <a href="#">View</a>
+                </li>
+                <li>
+                    <a href="#">Create</a>
+                </li>
+                <li>
+                    <a href="#">Settings</a>
+                </li>
+                <li>
+                    <a href="#">Logout</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1>Upload Manifest Page</h1>
+                        <p>
+                            <form action='insert.php' method='POST' enctype='multipart/form-data'>
+                                <input type='file' accept=".json" name='manifest'><br>
+                                <input type='submit' name='upload_btn' value='Upload'>
+                            </form>
+                        </p>
+                        <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+
+</html>
